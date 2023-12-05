9f67473f5ff9a3e5780e5e5426288d2892105f21
RAHUL <142935130+rahulkhatri3@users.noreply.github.com>
2023-12-06 00:45:07 +0530

    import

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("script is working from a diffrent file");
+//input/
+var username = prompt("give me username")
+
+//processing//
+var welcomemessage ="this script work"+ username;
+
+
+//output//
+alert(welcomemessage);
